bd4fa92a3321e10b1db30f762f411302b3e3aa48
dmtors <derektorsani@gmail.com>
2016-05-24 07:17:54 -0400

    tightened up code

--- xvx.js
+++ xvx.js
@@ -16,6 +16,13 @@ var setScore = function (team, score) {
   }
 }
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 $(document).ready(function() {
   setPointsToWin();
 });
